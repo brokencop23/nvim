f249f74676e9c622d877ba7dba261bfcecd862b4
d.emelyanov <d.emelyanov@betby.com>
2024-09-13 07:58:58 +0200

    fix

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,16 +1,24 @@
 -- Set leader key
 vim.g.mapleader = ' '
 
--- Key mappings
+-- Key mappings telescope
 vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fg', ':Telescope live_grep<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope help_tags<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope harpoon marks<CR>', { noremap = true, silent = true })
+
 vim.api.nvim_set_keymap('n', '<leader>q', ':Neotree float<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>u', '<cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>bb', ':BufferLinePick<CR>', {noremap=true, silent=true})
 vim.api.nvim_set_keymap('n', '<leader>bc', ':bd<CR>', {noremap=true, silent=true})
+
+
+-- Keymaps for harpoon
+vim.api.nvim_set_keymap('n', '<leader>hh', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', {noremap=true, silent=true})
+vim.api.nvim_set_keymap('n', '<leader>hm', ':lua require("harpoon.mark").add_file()<CR>', {noremap=true, silent=true})
+vim.api.nvim_set_keymap('n', '<leader>hn', ':lua require("harpoon.ui").nav_next()<CR>', {noremap=true, silent=true})
+vim.api.nvim_set_keymap('n', '<leader>hp', ':lua require("harpoon.ui").nav_prev()<CR>', {noremap=true, silent=true})
 
 
 -- Keymaps for debugging
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -3,5 +3,7 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         require("harpoon").setup({ })
+
+        require("telescope").load_extension("harpoon")
     end 
 }
